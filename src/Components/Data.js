669cd69a66aad75baf98d6dd8a82c4d8b62ba53c
Ghofran Louhaichi <gofilouhaichi@gmail.com>
2022-11-01 14:05:18 +0100

    Filling in Data.js with the necessary data to render in the cars component

---
+++ src/Components/Data.js
@@ -0,0 +1,31 @@
+const DataTable = [
+    {
+        id:1,
+        coverImg:"/imageP/japan.png",
+        location: 'Japan',
+        placeName: 'Mount Fuji',
+        period: '12 Jan, 2021 - 24 Jan, 2021',
+        description: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+        
+    },
+    {
+        id:2,
+        coverImg:"/imageP/australia.png",
+        location: 'Australia',
+        placeName: 'Sydney Opera House',
+        period: '27 May, 2021 - 8 Jun, 2021',
+        description: 'The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th centurys most famous and distinctive buildings',
+        
+    },
+    {
+        id:3,
+        coverImg:"/imageP/norway.png",
+        location: 'Norway',
+        placeName: 'Geirangerfjord',
+        period: '01 Oct, 2021 - 18 Nov, 2021',
+        description: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
+        
+    }
+];
+
+export default DataTable;
