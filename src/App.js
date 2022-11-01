a723cbbb3cb6e716dcbbcb05b4dc0fba16f2d75d
Ghofran Louhaichi <gofilouhaichi@gmail.com>
2022-11-01 23:03:24 +0100

    Final Version

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
       <Navbar />
       {arrPlaces}
     </div>
+    
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,8 @@ import global from '../imagesS/global.png'
 export default function Navbar() {
     return (
         <nav className='navbar'>
-            <h3><img alt='global' src={global} />My travel journal</h3>
+            <img alt='global' src={global} />
+            <h3>My travel journal</h3>
         </nav>
     )
 }
